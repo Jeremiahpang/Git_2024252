c67b9cdcc39c6d41f59b0d37396c26eb5c4fd002
Jeremiahpang <orphanyy@gmail.com>
2024-12-26 13:28:27 +0000

    Added a simple print

---
+++ src/git_2024252/Git_2024252.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2024252;
+
+/**
+ *
+ * @author jeremiah pang
+ */
+public class Git_2024252 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Jeremiah Pang");
+        System.out.println("Student ID: 2024252");
+    }
+    
+}
